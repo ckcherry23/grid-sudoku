ad6f5fef6f03dff8e279a968aac841de2f7e7686
Charisma Kausar <charisma.kausar@gmail.com>
2024-07-08 02:42:58 +0800

    feat: add sudoku class and types

---
+++ src/types/types.ts
@@ -0,0 +1,9 @@
+export type SudokuGrid = Array<Array<PossibleValue | null>>
+
+export type PossibleValue = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
+
+export type Move = {
+  row: PossibleValue
+  col: PossibleValue
+  value: PossibleValue | null
+}
